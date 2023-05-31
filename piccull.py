4dac37d13d92a921b318c59eb7298d5c3218b75f
Chifunt <chifunt@gmail.com>
2023-05-31 14:04:58 +0100

    feat: add culled image directory
    Instead of deleting the images, the application now moves them into a folder

--- piccull.py
+++ piccull.py
@@ -2,14 +2,16 @@ from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import filedialog
 import os
+import shutil
 
-class ImagePreviewer:
+class PicCull:
     def __init__(self, master):
         self.master = master
         self.master.title('PicCull')
         
         self.index = 0
         self.image_paths = []
+        self.culled_dir = None
 
         self.img_label = Label(master)
         self.img_label.pack()
@@ -19,7 +21,7 @@ class ImagePreviewer:
         frame = Frame(master)
         frame.pack()
 
-        Button(frame, text="Delete", command=self.delete_image).grid(row=0, column=0)
+        Button(frame, text="Cull", command=self.cull_image).grid(row=0, column=0)
         Button(frame, text="<- Prev", command=self.prev_image).grid(row=0, column=1)
         Button(frame, text="Next ->", command=self.next_image).grid(row=0, column=2)
         Button(frame, text="Skip", command=self.skip_image).grid(row=0, column=3)
@@ -27,12 +29,15 @@ class ImagePreviewer:
         self.master.bind('<Left>', lambda e: self.prev_image())
         self.master.bind('<Right>', lambda e: self.next_image())
         self.master.bind('<Up>', lambda e: self.skip_image())
-        self.master.bind('<Down>', lambda e: self.delete_image())
+        self.master.bind('<Down>', lambda e: self.cull_image())
 
     def open_directory(self):
         self.index = 0
         directory_path = filedialog.askdirectory(initialdir="/", title="Select a Directory")
         if directory_path:
+            self.culled_dir = os.path.join(directory_path, 'pic-culled')
+            if not os.path.exists(self.culled_dir):
+                os.makedirs(self.culled_dir)
             self.image_paths = list(filter(lambda f: f.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')), os.listdir(directory_path)))
             self.image_paths = [os.path.join(directory_path, f) for f in self.image_paths]
             self.show_image()
@@ -49,9 +54,9 @@ class ImagePreviewer:
             self.img_label.configure(image=None)
             self.img_label.image = None
 
-    def delete_image(self):
+    def cull_image(self):
         if self.index < len(self.image_paths):
-            os.remove(self.image_paths[self.index])
+            shutil.move(self.image_paths[self.index], self.culled_dir)
             del self.image_paths[self.index]
             self.show_image()
 
